a9e4339a77b189456d0a32f8bc9f15e4303b247c
austinahmau <89604674+austinahmau@users.noreply.github.com>
2022-12-12 00:05:49 -0800

    Used ADC code from lab 8 to test water sensor

--- CPE_final.ino
+++ CPE_final.ino
@@ -5,34 +5,52 @@
 #include <Stepper.h>
 
 
+#define RDA 0x80
+#define TBE 0x20
+
+//Temperature and Humidity Initialization
 #define DHTPIN 6
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-//rs = pin 7, e = pin 8, d4 = pin 9, d5 = pin 10, d6 = pin 11, d7 = pin 12 
-LiquidCrystal lcd(7, 8, 9, 10 ,11 , 12);
+//LCD Initialization
+LiquidCrystal lcd(7, 8, 9, 10 ,11 , 12); //rs = pin 7, e = pin 8, d4 = pin 9, d5 = pin 10, d6 = pin 11, d7 = pin 12 
 
+//Port A registers
 volatile unsigned char* port_a =(unsigned char*) 0x22;
 volatile unsigned char* ddr_a =(unsigned char*) 0x21;
 volatile unsigned char* pin_a =(unsigned char*) 0x20;
 
+//Port C registers
 volatile unsigned char* port_c =(unsigned char*) 0x28;
 volatile unsigned char* ddr_c =(unsigned char*) 0x27;
 volatile unsigned char* pin_c =(unsigned char*) 0x26;
 
+//ADC registers
 volatile unsigned char* my_ADMUX = (unsigned char *) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char *) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char *) 0x7A;
-volatile unsigned char* my_ADC_DATA = (unsigned char *) 0x78;
+volatile unsigned int* my_ADC_DATA = (unsigned int *) 0x78;
 
+//UART registers
+volatile unsigned char *myUCSR0A = (unsigned char *) 0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *) 0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *) 0x00C2;
+volatile unsigned int *myUBRR0 = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0 = (unsigned char *) 0x00C6;
+
+//clock instantiation
 RTC_DS1307 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+//stepper motor instantiation
 const int stepsPerRevolution = 32;
 Stepper myStepper = Stepper(stepsPerRevolution, 2, 4, 3, 5);
 
+unsigned int waterLevel = 0;
+
 void setup() {
-  Serial.begin(9600);
+  U0init(9600);
   adc_init();
 
   //set up LCD and DHT
@@ -53,54 +71,10 @@ void setup() {
 
 }
 
-void loop() {
-    bool clockwise = *pin_c & 0b01000000; //left button
-    bool anticlockwise = *pin_c & 0b10000000; //right button
-
-    if(clockwise){
-      myStepper.setSpeed(200);
-      myStepper.step(50);
-      Serial.print("Vent up at: ");
-      timeStamp();
-    }
-    else if(anticlockwise){
-      myStepper.setSpeed(200);
-      myStepper.step(-50);
-      Serial.print("Vent down at: ");
-      timeStamp();
-    }
-}
-
-void adc_init(){
-  *my_ADCSRA |= 0b10000000; //set bit 7 to 1 to enable ADC
-  *my_ADCSRA &= 0b11011111; //clear bit 5 to 0 to disable ADC trigger mode and interrupt, and set prescalar selection to slow reading
-
-  *my_ADCSRB &= 0b11110000; //clear bit 3 to 0 to reset the channel and gain bits and clear bits 2-0 to set to free running mode
-
-  *my_ADMUX &= 0b01111111; //clear bit 7 to 0 for AVCC analog reference
-  *my_ADMUX |= 0b01000000; //set bit 6 to 1 for AVCC analog reference
-  *my_ADMUX &= 0b11011111; //clear bit 5 to 0 for right adjust result
-  *my_ADMUX &= 0b11100000; //clear bits 4-0 to reset the channel and gain bits
-}
-
-unsigned int adc_read(unsigned char adc_channel_num){
-  *my_ADMUX &= 0b11100000; //clear the channel selection bits
-  *my_ADCSRB &= 0b11011111; //clear the channel selection bits
-
-  //set channel selection bits, but remove the MSB (bit 3)
-  if(adc_channel_num > 7){
-    adc_channel_num -= 8;
-    *my_ADCSRB |= 0b00100000;
-  }
-
-  *my_ADMUX += adc_channel_num; //set channel selection bits
-  *my_ADCSRA |= 0b01000000; //set bit 6 of ADCSRA to 1 to start conversion
-
-  //wait for conversion to complete
-  while((*my_ADCSRA & 0b01000000) != 0){
-    return *my_ADC_DATA; //return result to ADC data register
-  }
-  
+void loop(){
+  /*unsigned int waterReading = adc_read(0);
+  print_int(waterReading);
+  delay(500);*/
 }
 
 void LCD_display(){
@@ -146,6 +120,23 @@ void timeStamp(){
   Serial.println();
 }
 
+void ventDirection(){
+  bool clockwise = *pin_c & 0b01000000; //left button
+  bool anticlockwise = *pin_c & 0b10000000; //right button
+  
+  if(clockwise){
+    myStepper.setSpeed(200);
+    myStepper.step(50);
+    Serial.print("Vent up at: ");
+    timeStamp();
+  }
+  else if(anticlockwise){
+    myStepper.setSpeed(200);
+    myStepper.step(-50);
+    Serial.print("Vent down at: ");
+    timeStamp();
+  }
+}
 void disabledMode(){
   *pin_a |= 0b00000001; //yellow LED pin 22
   
@@ -161,4 +152,103 @@ void runningMode(){
 
 void errorMode(){
   *pin_a |= 0b00001000; //red LED pin 25
+}
+
+void adc_init(){
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+
+unsigned int adc_read(unsigned char adc_channel_num){
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11011111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00100000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+  
+}
+
+void print_int(unsigned int out_num){
+  // clear a flag (for printing 0's in the middle of numbers)
+  unsigned char print_flag = 0;
+  // if its greater than 1000
+  if(out_num >= 1000)
+  {
+    // get the 1000's digit, add to '0' 
+    U0putchar(out_num / 1000 + '0');
+    // set the print flag
+    print_flag = 1;
+    // mod the out num by 1000
+    out_num = out_num % 1000;
+  }
+  // if its greater than 100 or we've already printed the 1000's
+  if(out_num >= 100 || print_flag)
+  {
+    // get the 100's digit, add to '0'
+    U0putchar(out_num / 100 + '0');
+    // set the print flag
+    print_flag = 1;
+    // mod the output num by 100
+    out_num = out_num % 100;
+  } 
+  // if its greater than 10, or we've already printed the 10's
+  if(out_num >= 10 || print_flag)
+  {
+    U0putchar(out_num / 10 + '0');
+    print_flag = 1;
+    out_num = out_num % 10;
+  } 
+  // always print the last digit (in case it's 0)
+  U0putchar(out_num + '0');
+  // print a newline
+  U0putchar('\n');
+}
+
+void U0init(int U0baud){
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+
+unsigned char U0kbhit(){
+  return *myUCSR0A & RDA;
+}
+
+unsigned char U0getchar(){
+  return *myUDR0;
+}
+
+void U0putchar(unsigned char U0pdata){
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
 }
